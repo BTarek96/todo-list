7a451ebf6533d9c56ad7f6ccc43b086958ffff09
Bassem Tarek <btarek9616@gmail.com>
2022-11-07 17:27:30 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import TaskList from "./Components/TaskList";
 import { useState } from "react";
 
 function App() {
+  /* const [focused, setFocused] = useState(false); */
   const [list, setList] = useState([{ title: "." }]);
+  const [value, setValue] = useState("");
 
   const addTask = (newTask) => {
-    setList((current) => [...current, newTask]);
+    const nTask = { title: String(newTask) };
+    setList((current) => [...current, nTask]);
+    console.log(list);
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTask(event.target.value);
   };
 
   return (
     <div className="App">
       <h1 className="Title">Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={handleChange}
+          className="taskInput"
+        />
+      </form>
       <TaskList list={list} onAddTask={addTask} />
     </div>
   );
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,18 +1,9 @@
-import { Input } from "@mantine/core";
-import CheckBox from "./CheckBox";
 const TaskList = ({ list, onAddTask }) => {
   return (
     <div>
-      <Input
-        className="taskInput"
-        placeholder="New Task"
-        radius="md"
-        size="lg"
-      />
       <ul id="tasklist">
         {list.map((task) => (
           <li key={task.title} className="task">
-            <CheckBox />
             {task.title}
           </li>
         ))}
